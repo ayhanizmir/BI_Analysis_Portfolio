e3b7dc88d261c3250a533e27664d3f356f0b369a
ayhanizmir <41268517+ayhanizmir@users.noreply.github.com>
2021-07-01 12:24:38 +0300

    Add files via upload

---
+++ Making Function Script Example.sql
@@ -0,0 +1,25 @@
+# Create a function called ‘emp_info’ that takes for parameters the first and last name of an employee
+# and returns the salary from the newest contract of that employee.
+
+USE employees;
+DROP FUNCTION IF EXISTS emp_info;
+
+DELIMITER $$
+CREATE FUNCTION emp_info(p_first_name varchar(255), p_last_name varchar(255)) RETURNS decimal(10,2)
+DETERMINISTIC
+BEGIN
+	DECLARE v_max_from_date date;
+    DECLARE v_salary decimal(10,2);
+SELECT
+	MAX(from_date) INTO v_max_from_date FROM employees e
+		JOIN salaries s ON e.emp_no = s.emp_no
+			WHERE e.first_name = p_first_name AND e.last_name = p_last_name;
+SELECT 
+	s.salary INTO v_salary FROM employees e
+		JOIN salaries s ON e.emp_no = s.emp_no
+			WHERE e.first_name = p_first_name AND e.last_name = p_last_name AND s.from_date = v_max_from_date;
+RETURN v_salary;
+END$$
+DELIMITER ;
+
+SELECT EMP_INFO('Aruna', 'Journel');
